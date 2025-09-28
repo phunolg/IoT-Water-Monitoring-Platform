d509a8ede1ddb470dd4bc4175d64279475c0008a
Phuong Tran <phuong0968804740@gmail.com>
2025-09-28 22:39:04 +0700

    Fix linting errors

--- water_monitor/urls.py
+++ water_monitor/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
-                                   SpectacularSwaggerView)
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 from monitoring import views as monitoring_views
 
