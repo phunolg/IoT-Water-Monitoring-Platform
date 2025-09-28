94a82b49428195e52830efddf8e7958409ec0a47
Phuong Tran <phuong0968804740@gmail.com>
2025-09-28 23:22:49 +0700

    Fix linting errors

---
+++ water_monitor/test_settings.py
@@ -0,0 +1,48 @@
+# Test settings for Django
+import os
+from .settings import *
+
+# Override settings for testing
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+# Disable migrations for faster testing
+class DisableMigrations:
+    def __contains__(self, item):
+        return True
+    
+    def __getitem__(self, item):
+        return None
+
+MIGRATION_MODULES = DisableMigrations()
+
+# Speed up password hashing
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+# Disable logging during tests
+LOGGING_CONFIG = None
+
+# Media files for testing
+MEDIA_ROOT = '/tmp/test_media/'
+
+# Email backend for testing
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+# Cache for testing
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
+# Test runner
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+# Debug
+DEBUG = True
